26604d1eb7270884206ce2a8223906a5982bd862
SharkChili <510763668@qq.com>
2022-07-24 23:04:46 +0800

    六大基本原则--接口隔离原则

---
+++ src/main/java/com/rjxy/isolation/fresh/ISkillInvisible.java
@@ -0,0 +1,14 @@
+package com.rjxy.isolation.fresh;
+
+/**
+ * @Description ISkillInvisible
+ * @Author 蜻蜓队长 隐藏接口
+ * @Date 2022/7/24 22:49
+ * @Version 0.1
+ */
+public interface ISkillInvisible {
+    /**
+     * 隐藏
+     */
+    void doInvisible();
+}
